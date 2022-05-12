9bc922b7d9cba0c113b40f5b55eb8cdb1c203d46
Neil Janwani <kpochana@scratpi>
2022-05-12 13:04:53 +0100

    poop

--- scripts/planning/localize.py
+++ scripts/planning/localize.py
@@ -20,21 +20,26 @@ from PlanarTransform import *
 
 from sensor_msgs.msg import JointState, LaserScan
 from nav_msgs.msg      import Odometry, OccupancyGrid
-from geometry_msgs.msg import PoseStamped, Point, Quaternion, Twist, Pose, TransformStamped, PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseArray, PoseStamped, Point, Quaternion, Twist, Pose, TransformStamped, PoseWithCovarianceStamped
 
 
 WALLTHRESHOLD = 0.1
-FRACTIONAL_SCALE = 1.0 / 1000.0
+FRACTIONAL_SCALE = 1.0
+FRACTIONAL_SCALE_THETA = 1.0
+MAX_CART_SCALE = 0.03
+MAX_THETA_SCALE = np.pi / 20.0
+THROTTLE = 0.0005
 GRID_X = -3.8100
 GRID_Y = -3.8100
 GRID_THETA = 0.0
+
 
 class Localize: 
 
     def __init__(self):
         print("Initializing localization...")
         self.counter = 0
-        self.localize_rate = 10
+        self.localize_rate = 25
         self.initialized = False
         # 4.1
         self.pt_map_odom = PlanarTransform.unity()
@@ -45,12 +50,6 @@ class Localize:
         self.T_odom_base = self.pt_odom_base.toTransform()
         self.pt_map_base = PlanarTransform.unity()
         self.T_map_base = self.pt_map_base.toTransform()
-
-        # Set up publishers/subscribers
-        rospy.Subscriber("/odom", Odometry, self.cb_odom)
-        self.pose_pub = rospy.Publisher("/pose", PoseStamped, queue_size=10)
-        rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.cb_initial_pose)
-        rospy.Subscriber("/scan", LaserScan, self.cb_laser)
 
         # 4.2
         self.mapmsg = None
@@ -75,11 +74,42 @@ class Localize:
         # print("Dims of wallpts: ", self.wallpts.shape)
         print("Found Neighbors")
         self.wallptmap = np.zeros((self.h, self.w, 2))
-        print("Setting up wallptmap")
+        # print("Setting up wallptmap")
+        # for v in range(self.h):
+        #     print(str(round((v * self.w) / (self.h * self.w) * 100, 2)) + " done")
+        #     for u in range(self.w):
+        #         self.wallptmap[v, u] = self.calculate_point(u, v)
+
+         
+        w_array = []
+        # for v in range(self.h):
+        #     for u in range(self.w):
+        #         w_array.append(self.wallptmap[v, u][0])
+        #         w_array.append(self.wallptmap[v, u][1])
+        # print('Writing file using array with dimensions', np.shape(w_array), '...')
+        # np.savetxt('/home/kpochana/robotws/src/me169/data/wallptmap.csv', w_array, delimiter=',')
+        # print('Done! :)')
+
+        print("Reading file...")
+        w_array = np.genfromtxt('/home/kpochana/robotws/src/me169/data/wallptmap.csv', delimiter=',')
+
+        # Set up publishers/subscribers
+        rospy.Subscriber("/odom", Odometry, self.cb_odom)
+        self.pose_pub = rospy.Publisher("/pose", PoseStamped, queue_size=10)
+        rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.cb_initial_pose)
+        rospy.Subscriber("/scan", LaserScan, self.cb_laser)
+        self.array_pub = rospy.Publisher("/posearray", PoseArray)
+
+
+        i = 0
         for v in range(self.h):
-            print(str(round((v * self.w) / (self.h * self.w) * 100, 2)) + " done")
             for u in range(self.w):
-                self.wallptmap[v, u] = self.calculate_point(u, v)
+                self.wallptmap[v, u][0] = w_array[i]
+                self.wallptmap[v, u][1] = w_array[i + 1]
+                i += 2
+
+        print('Made array with dimensions', np.shape(self.wallptmap), '...')
+        print('Done :)')
 
         # with open("/home/kpochana/robotws/src/me169/data/wallptmap.csv", 'w') as csvfile:
         #     writer = csv.writer(csvfile, delimiter=",",lineterminator="\n")
@@ -87,7 +117,7 @@ class Localize:
         #         print("Writing " + str(round((v * self.w) / (self.h * self.w) * 100, 2)) + " done")
         #         writer.writerow(self.wallptmap[v,:])
 
-        print("Set up wallptmap")
+        print("Set up wallptmap!")
         self.d_theta = 0.0
         self.d_x = 0.0
         self.d_y = 0.0
@@ -177,20 +207,20 @@ class Localize:
             self.counter += 1
             self.pt_odom_laser = PlanarTransform.fromTransform(tfmsg.transform)
             # print(self.pt_odom_laser.x(), self.pt_odom_laser.y())
-            self.pt_map_laser = self.pt_map_odom * self.pt_odom_laser
+            self.pt_map_laser = self.pt_map_odom * self.pt_odom_laser #* PlanarTransform.basic(0.0, 0.0, -np.pi / 2.0)
             # print(self.pt_map_laser)
             
             if (self.contlocal and self.counter % self.localize_rate == 0):
-                print("localizing...")
+                # print("localizing...")
                 r = np.zeros((2, 0))
                 p = np.zeros((2, 0))
                 ranges = msg.ranges
-                angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
+                angles = np.arange(msg.angle_min, msg.angle_max + msg.angle_increment, msg.angle_increment)
 
                 for i, range in enumerate(ranges):
-                    if range > msg.range_min and range < (msg.range_max / 1.0):
-                        x = range * np.sin(angles[i])
-                        y = range * np.cos(angles[i])
+                    if range > msg.range_min and range < (msg.range_max / 2.0):
+                        y = range * np.sin(angles[i])
+                        x = range * np.cos(angles[i])
                         # print("x: ", x)
                         # print("y: ", y)
                         pt_r = self.pt_map_laser * PlanarTransform(x, y, 0.0, 1.0)
@@ -201,19 +231,45 @@ class Localize:
                         # r_y = self.map_to_grid(pt_r.y())
                         # print("r_x_g: ", r_x)
                         # print("r_y_g: ", r_y)
-                        r = np.hstack([r, np.array([[r_x], [r_y]])])
-                        u = int(round((r_x - GRID_X) / self.resolution))
-                        v = int(round((r_y - GRID_Y) / self.resolution))
-                        # try:
-                        p_uv = self.nearestwallpt(u, v)
-                        p_x = float(p_uv[0]) * self.resolution + GRID_X
-                        p_y = float(p_uv[1]) * self.resolution + GRID_Y
-                        p = np.hstack([p, np.array([[p_x], [p_y]])])
+                        u = int((r_x - GRID_X) / self.resolution)
+                        v = int((r_y - GRID_Y) / self.resolution)
+                        try:
+                            p_uv = self.nearestwallpt(u, v)
+                            p_x = float(p_uv[0]) * self.resolution + GRID_X
+                            p_y = float(p_uv[1]) * self.resolution + GRID_Y
+                            p = np.hstack([p, np.array([[p_x], [p_y]])])
+                            r = np.hstack([r, np.array([[r_x], [r_y]])])
+                        except:
+                            continue
+                
+                poses = []
+                for wallpt in np.swapaxes(p, 0, 1):
+                    pose = Pose()
+                    pose.position = Point(wallpt[0], wallpt[1], 0.0)
+                    pose.orientation = Quaternion(0.0, 0.0, math.sin(0.0), math.cos(0.0))
+                    poses.append(pose)
 
+                poses_msg = PoseArray()
+                poses_msg.header.stamp = rospy.Time.now()
+                poses_msg.header.frame_id = 'map'
+                poses_msg.poses = poses
+
+                self.array_pub.publish(poses_msg)
                 self.minimize_least_squares(r, p)
 
                 # # get the map to base frame from initial pose
-                self.pt_map_base = self.pt_map_base * (FRACTIONAL_SCALE * PlanarTransform.basic(-self.d_x, -self.d_y, -self.d_theta))
+                dx = self.bound(MAX_CART_SCALE, self.d_x * FRACTIONAL_SCALE)
+                dy = self.bound(MAX_CART_SCALE, self.d_y * FRACTIONAL_SCALE)
+                dtheta = self.bound(MAX_THETA_SCALE, self.d_theta * FRACTIONAL_SCALE_THETA)
+                if (np.linalg.norm(np.array([dx, dy, dtheta])) < THROTTLE):
+                    dx = 0.0
+                    dy = 0.0
+                    dtheta = 0.0
+                print(dx, dy, dtheta)
+                # print("BEFORE:", self.pt_map_base)
+                self.pt_map_base = PlanarTransform.basic(dx, dy, 0.0) * self.pt_map_base * PlanarTransform.basic(0.0, 0.0, dtheta)
+                # print("AFTER:", self.pt_map_base, "\n---")
+
                 self.T_map_base = self.pt_map_base.toTransform()
 
                 # back-calculate? the transform for the odom in map frame (or is it reversed...)
@@ -233,6 +289,12 @@ class Localize:
     def get_theta(self, q):
         x = 2.0 * np.arctan2(q.z, q.w)
         return x
+
+    def bound(self, magnitude, val):
+        assert(magnitude >= 0)
+        if val < 0:
+            return max(-1.0 * magnitude, val)
+        return min(magnitude, val)
 
     def map_to_grid(self, x, y):
         x_g = np.cos(GRID_THETA) * (x - GRID_X) + np.sin(GRID_THETA) * (y - GRID_Y)
@@ -256,14 +318,14 @@ class Localize:
         return self.wallptmap[v, u]
 
     def minimize_least_squares(self, r: np.array, p: np.array):
-        assert(len(r) == len(p))
-
-        N = len(r)
+        # assert(len(r) == len(p))
 
         rx = r[0, :]
         ry = r[1, :]
         px = p[0, :]
         py = p[1, :]
+
+        N = float(len(rx))
 
         Rx = 1.0 / N * np.sum(rx)
         Ry = 1.0 / N * np.sum(ry)
